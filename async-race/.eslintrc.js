99453af2a51ffd13f18bd4549d4e1f4776267a40
gorobchenko-k <gorobchenko.es@mail.ru>
2023-07-22 20:21:24 +0300

    chore: add pritter

--- async-race/.eslintrc.js
+++ async-race/.eslintrc.js
@@ -5,18 +5,21 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'plugin:prettier/recommended',
+    'prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: ['import'],
+  plugins: ['import', 'prettier'],
   rules: {
     'global-require': 'error',
     'import/extensions': ['error', 'always', { ts: 'never' }],
     'import/prefer-default-export': 'off',
     'class-methods-use-this': 'off',
     'no-use-before-define': ['error', { variables: false }],
+    'prettier/prettier': 'error',
   },
 };
