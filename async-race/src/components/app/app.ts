b0db7a533bdc0e08939350b26389ccd0fd201739
gorobchenko-k <gorobchenko.es@mail.ru>
2023-07-22 20:47:40 +0300

    feat: add main components

--- async-race/src/components/app/app.ts
+++ async-race/src/components/app/app.ts
@@ -1,8 +1,11 @@
 import { Header } from '../header/header';
+import { Main } from '../pages/main';
 import { Footer } from '../footer/footer';
 
 class App {
   private header = new Header();
+
+  private main = new Main();
 
   private footer = new Footer();
 
---
+++ async-race/src/components/pages/main.ts
@@ -0,0 +1,29 @@
+import { createElement } from '../../helpers';
+import { Garage } from './garage/garage';
+import './main.css';
+import { Winner } from './winner/winner';
+
+const MAIN_STYLE = {
+  main: ['main'],
+  container: ['main__container', 'container'],
+};
+
+class Main {
+  private garagePage = new Garage();
+
+  private winnerPage = new Winner();
+
+  constructor() {
+    this.createMain();
+  }
+
+  private createMain(): void {
+    const main = createElement('main', MAIN_STYLE.main);
+    const container = createElement('div', MAIN_STYLE.container);
+    container.append(this.garagePage.createGarage(), this.winnerPage.createWinner());
+    main.append(container);
+    document.body.append(main);
+  }
+}
+
+export { Main };
