9cfa4d2023a2c1c756698de987e7e3ba69963168
gorobchenko-k <gorobchenko.es@mail.ru>
2023-07-22 20:41:57 +0300

    feat: add footer component

--- async-race/src/components/app/app.ts
+++ async-race/src/components/app/app.ts
@@ -1,7 +1,10 @@
 import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
 
 class App {
   private header = new Header();
+
+  private footer = new Footer();
 
   public start(): void {}
 }
