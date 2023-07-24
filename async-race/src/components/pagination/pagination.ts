8c6e8be6c1a036f99a1d0f440da4827403755d4a
gorobchenko-k <gorobchenko.es@mail.ru>
2023-07-24 14:27:41 +0300

    feat: add checking disable of buttons

--- async-race/src/components/pagination/pagination.ts
+++ async-race/src/components/pagination/pagination.ts
@@ -13,17 +13,21 @@ class Pagination {
 
   private nextButton: HTMLButtonElement;
 
+  private action: () => void;
+
   constructor(
     limitItemsPerPage: number,
     elementOfCurrentPage: HTMLSpanElement,
     prevButton: HTMLButtonElement,
-    nextButton: HTMLButtonElement
+    nextButton: HTMLButtonElement,
+    action: () => void
   ) {
     this.limitItemsPerPage = limitItemsPerPage;
     this.elementOfCurrentPage = elementOfCurrentPage;
     this.prevButton = prevButton;
     this.nextButton = nextButton;
     this.elementOfCurrentPage.textContent = `${this.currentPage}`;
+    this.action = action;
     this.addButtonHandler();
   }
 
@@ -36,19 +40,37 @@ class Pagination {
     if (this.currentPage < this.numberOfPages) {
       this.currentPage += 1;
       this.elementOfCurrentPage.textContent = `${this.currentPage}`;
+      this.checkDisabledButton();
+      this.action();
     }
   }
 
   private prevPage(): void {
-    if (this.currentPage > 1) {
+    if (this.currentPage - 1 > 0) {
       this.currentPage -= 1;
       this.elementOfCurrentPage.textContent = `${this.currentPage}`;
+      this.checkDisabledButton();
+      this.action();
     }
   }
 
   public setNumberOfItems(numberOfItems: number): void {
     this.numberOfItems = numberOfItems;
     this.numberOfPages = Math.ceil(this.numberOfItems / this.limitItemsPerPage);
+    this.checkDisabledButton();
+  }
+
+  private checkDisabledButton(): void {
+    if (this.currentPage - 1 === 0) {
+      this.prevButton.disabled = true;
+    } else {
+      this.prevButton.disabled = false;
+    }
+    if (this.currentPage === this.numberOfPages) {
+      this.nextButton.disabled = true;
+    } else {
+      this.nextButton.disabled = false;
+    }
   }
 }
 
